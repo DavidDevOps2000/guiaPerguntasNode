67147fe7d4b7eb30365dd644f17705730646e4ce
DavidDevops2000 <DavidDevops2000@gmail.com>
2020-11-07 00:47:37 -0300

    fixing the error on render index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.set('view', 'ejs');
+app.set('view engine', 'ejs');//EJS ajuda a redenrizar interfaces na tela, onde o mesmo cria front com HTML, 
+                        //e apara SETA-LO usamo o metodo set do expressps
+                        //sempre tem que hgaver com pasta views por obrigação
 
-app.get('/', function(req, res){
-    res.send("Aqui foi");
+app.get('/', (req, res)=>{
+    /*res.send("Aqui foi");*/
+    res.render('index')//repare que esse arquivo index é o index.ejs e não index.js e vc nem precisa colocar o caminho alem do nome
 });
 
-app.listen(8080, function(erro){
-    if(erro){
-        console.log("problemas aqui");
+app.listen(8080,(erro) =>{
+    if(erro)
+    {   console.log("problemas aqui");
     }else{
         console.log("running");
-    }
-    
+        }
 });
